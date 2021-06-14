a20a524355819a7ab1540047f798fb09ee691466
daxit99 <73168093+daxit99@users.noreply.github.com>
2021-06-15 01:17:38 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('daxitsurani140@gmail.com')
+    MAIL_PASSWORD = os.environ.get('Daxit983@')
+    ADMINS = ['daxitsurani140@gmail.com']
+    POSTS_PER_PAGE = 25
+   
